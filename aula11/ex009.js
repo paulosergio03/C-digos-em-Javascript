3521ced235bcd0770b9a5752875bb4543e162c44
paulosergio03 <paulosergioadm9@gmail.com>
2024-07-31 19:18:54 -0300

    Primeiro commit

---
+++ aula11/ex009.js
@@ -0,0 +1,15 @@
+/*var país = "França";
+console.log(`Vivendo em ${país}`);
+if (país != "Brasil") {
+  console.log("Você é Estrageiro!");
+} else {
+  console.log("Você é Brasileiro!");
+} */
+
+const listaDePaíses = ["França", "Alemanhã", "Brasil"];
+var primeiroItem = listaDePaíses[0]; // Pega o item 'Maçãs'
+var segundoItem = listaDePaíses[1]; // Pega o item 'Maçãs'
+var terceiroItem = listaDePaíses[2]; // Pega o item 'Maçãs'
+var país = listaDePaíses
+
+console.log(países[0])
